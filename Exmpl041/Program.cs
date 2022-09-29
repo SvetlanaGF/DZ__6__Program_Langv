f2d605505fffaef96a2d69b44df194e08275a017
Svetlana <svgf@mail.ru>
2022-09-30 00:28:00 +0300

    Задача 041 с рекурсией

--- Exmpl041/Program.cs
+++ Exmpl041/Program.cs
@@ -14,43 +14,50 @@ void InputArray (int [] arrInput)
     }
 }
 
-void OutputArray (int[] arrOutput)
-{  
-    Console.WriteLine($"\nСформирован массив из {arrOutput.Length} элементов.");
-    Console.Write($"[ ");
-
-    for (int i=0; i<arrOutput.Length; i++)
+static void OutputArray (int[] arrOutput, int i=0)
+{   
+    Console.Write($"{arrOutput[i]}");
+    i = i+1;
+    if (i < arrOutput.Length)  //Условие выхода из рекурсии
     {
-        Console.Write($"{arrOutput[i]}");
-        if (i<(arrOutput.Length-1)) Console.Write(", ");
+        if (i<=arrOutput.Length) 
+            {
+                Console.Write(", ");
+            }
+        OutputArray (arrOutput, i);
     }
-    Console.WriteLine(" ]");
 }
 
-void MetodAboveZero (int [] arr)
+//int [] arrayExmpl = {0, 7, 8, -2, -2};
+void MetodAboveZero (int [] arr, ref int i, ref int aboveZero)
 {
-    int aboveZero = 0;
-    for (int i=0; (i < arr.Length); i++)
-    {
-       if (arr [i] > 0) aboveZero += 1;
-    } 
-Console.Write($"В нём элементов больше 0 - {aboveZero}\n");
+        if (arr [i] > 0) 
+        {
+            aboveZero = aboveZero + 1;
+            i++;
+        }
+        else 
+        { 
+            i++;
+            if (i < arr.Length)
+            {
+                MetodAboveZero (arr, ref i, ref aboveZero);
+            }
+        }
 }
-
-// 0, 7, 8, -2, -2 -> 2
-int [] arrayExmpl1 = {0, 7, 8, -2, -2};
-OutputArray (arrayExmpl1);
-MetodAboveZero (arrayExmpl1); 
-
-// 1, -7, 567, 89, 223-> 3
-int [] arrayExmpl2 = {1, -7, 567, 89, 223};
-OutputArray (arrayExmpl2);
-MetodAboveZero (arrayExmpl2); 
 
 Console.Write("\nВведите количество элементов массива:  ");
 int countArray = int.Parse(Console.ReadLine());
 int [] array = new int [countArray];
+
 InputArray (array);
+Console.Write($"[ ");
 OutputArray (array);
-MetodAboveZero (array);
-Console.WriteLine();
+Console.WriteLine(" ]");
+int aboveZero = 0;
+int i = 0;
+while (i < array.Length)
+{
+    MetodAboveZero (array, ref i, ref aboveZero);
+}
+Console.WriteLine($"В этом массиве элементов больше 0 - {aboveZero}\n");
--- Exmpl043/Program.cs
+++ Exmpl043/Program.cs
@@ -2,21 +2,30 @@
 // y = k1 * x + b1, 
 // y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// k1 = 5, b1 = 2, k2 = 9, b2 = 4 -> (-0,5; -0,5)
 
 Console.Clear();
-//Console.Write("Введите значение k1: ");
-//Console.ReadLine(k1);
-//Console.Write("значение b1: ");
-//Console.ReadLine(b1);
-//Console.Write("значение k2: ");
-//Console.ReadLine(k2);
-//Console.Write("значение b2: ");
-//Console.ReadLine(b2);
+double [,] lines = { {5, 2}, { 9, 4}};
+double x1;
+double y1;
+
+if ((lines[0,0] == lines[1,0]) && (lines[0,1] == lines[1,1])) 
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else if (lines[0,0] == lines[1,0]) Console.WriteLine("Прямые параллельны");
+else
+{
+    //x=(b2-b1)/(k1-k2)
+    x1=(lines[1,1] - lines[0,1])/(lines[0,0] - lines[1,0]);
+    //y=(k1*(b2-b1))/(k1-k2)+b1
+    y1=(lines[0,0]*(lines[1,1] - lines[0,1]))/(lines[0,0] - lines[1,0])+lines[0,1];
+    Console.WriteLine($"x= {x1}, y= {y1}");
+}
+
 
 double b1 = 2, k1 = 5, b2 = 4, k2 = 9;
-double x;
-double y;
+double x, y;
 
 if ((k1 == k2) && (b1 == b2)) Console.WriteLine("Прямые совпадают");
 
